446c015984487364f3ef52172f85e7a63c47cd28
Semyon <semyonchristopher@gmail.com>
2022-10-22 08:43:25 -0500

    final commit

---
+++ model/strategypattern/SnakeMoveStrategy.java
@@ -0,0 +1,5 @@
+package model.strategypattern;
+
+public interface SnakeMoveStrategy {
+	void moveAlgorithm();
+}
---
+++ model/strategypattern/SnakeRenderStrategy.java
@@ -0,0 +1,7 @@
+package model.strategypattern;
+
+import java.awt.Graphics2D;
+
+public interface SnakeRenderStrategy {
+	void renderAlgorithm(Graphics2D g2);
+}
